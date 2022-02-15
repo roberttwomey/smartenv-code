29fe9dad036c1f125d036b5ac193c6db6928f122
Robert Twomey <robert.twomey@gmail.com>
2022-02-15 09:16:22 -0600

    Update launch_blescanner.sh

--- ble/launch_blescanner.sh
+++ ble/launch_blescanner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sleep 10
 cd /home/pi/smartenv-code/ble/
-/usr/bin/screen -dmS blescanner sudo /usr/bin/python3 /home/pi/smartenv-code/ble/ble_to_mqtt.py
+/usr/bin/screen -dmS blescanner sudo /usr/bin/python /home/pi/smartenv-code/ble/ble_to_mqtt.py
