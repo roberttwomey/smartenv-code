ebc2cda72ac261f237ad0b7a0ba1080bf9d3646e
Robert Twomey <robert.twomey@gmail.com>
2022-02-27 17:24:19 -0800

    Update speechrecognition_mqtt.py

--- respeaker/speechrecognition_mqtt.py
+++ respeaker/speechrecognition_mqtt.py
@@ -9,9 +9,31 @@
 # Plus some mqtt magic
 
 import speech_recognition as sr
+import paho.mqtt.client as paho
 
+# speech recognition  
 r = sr.Recognizer()
 m = sr.Microphone()
+
+# mqtt
+client1 = None
+
+clientName = "smartenv"
+broker_addr= "public.cloud.shiftr.io"
+#broker_addr= "34.77.13.55"
+broker_port = 443 # ignored, https not working right now
+topic = "smartenv/speech"
+mqtt_user = "public"
+mqtt_password = "public"
+
+client1 = paho.Client(clientName)
+client1.on_connect = on_connect
+# client1.on_publish = on_publish # doesn't seem to work
+client1.username_pw_set(mqtt_user, password=mqtt_password)
+print("connecting...", end="")
+client1.connect(broker_addr)
+print("done.")
+
 
 try:
     print("A moment of silence, please...")
@@ -30,6 +52,11 @@ try:
                 print(u"You said {}".format(value).encode("utf-8"))
             else:  # this version of Python uses unicode for strings (Python 3+)
                 print("You said {}".format(value))
+                
+                # publish to mqtt
+                ret = client1.publish(topic, value)
+                print("published {} to mqtt {}".format(topic, value)))
+                
         except sr.UnknownValueError:
             print("Oops! Didn't catch that")
         except sr.RequestError as e:
